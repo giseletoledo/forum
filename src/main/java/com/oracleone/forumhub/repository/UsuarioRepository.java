688fc61dd165135ef5cbcb160a488122e49cb7b1
Gisele <gisesonia@gmail.com>
2025-01-15 21:50:12 -0300

    mudando de diretorio a classe UsuarioRepository

--- src/main/java/com/oracleone/forumhub/repository/UsuarioRepository.java
+++ src/main/java/com/oracleone/forumhub/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
-package com.oracleone.forumhub.domain.usuario;
+package com.oracleone.forumhub.repository;
 
+import com.oracleone.forumhub.domain.usuario.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
